5bfec297b134b5b8f5a40ff83bdeadf3899607e2
Roman <bromanla.json@gmail.com>
2021-08-25 20:17:20 +0300

    Downgrade vue and element-plus (updates are unstable)

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 
 import elementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
+import 'element-plus/lib/theme-chalk/index.css'
 
 createApp(App)
   .use(store)
